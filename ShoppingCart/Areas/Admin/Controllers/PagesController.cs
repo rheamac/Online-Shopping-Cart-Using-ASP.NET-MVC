4e431302b7f7c895549e389ddf757572153a4dd5
rheamac <rheamac999@gmail.com>
2019-07-12 23:07:56 -0400

    Add project files.

---
+++ ShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,28 @@
+﻿using ShoppingCart.Models.Data;
+using ShoppingCart.Models.ViewsModel.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // Declare List of PageVM
+            List<PagesVM> pageList;
+
+            using(Db db = new Db())
+            {
+                // Initialize the list
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PagesVM(x)).ToList();
+            }
+            // return view with the list
+            return View(pageList);
+        }
+    }
+}
---
+++ ShoppingCart/Models/ViewsModel/Pages/PagesVM.cs
@@ -0,0 +1,36 @@
+﻿using ShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models.ViewsModel.Pages
+{
+    public class PagesVM
+    {
+        public PagesVM()
+        {
+
+        }
+        public PagesVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HashSideBar = row.HashSideBar;
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int .MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HashSideBar { get; set; }
+    }
+}
